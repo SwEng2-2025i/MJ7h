f831ea036fea5f851d3e9fdeea3f3d56e47714c2
Samuel Vargas <sjvc0595@gmail.com>
2025-05-24 19:22:25 -0500

    feat(user): Add from_dict and to_dict methods for User class

--- Laboratory1/1011201389/user.py
+++ Laboratory1/1011201389/user.py
@@ -4,4 +4,20 @@ class User():
         self.preferred_channel = preferred_channel
         self.available_channels = available_channels
 
+    # Factory method from a dict
+    @classmethod
+    def from_dict(cls, data):
+        name = data.get("name")
+        preferred_channel = data.get("preferred_channel")
+        available_channels = data.get("available_channels")
+
+        return User(name,  preferred_channel, available_channels)
+    
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "preferred_channel": self.preferred_channel,
+            "available_channels": self.available_channels
+        }
+
     
