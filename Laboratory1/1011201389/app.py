63c70c921c771739f6d456b6670cfa5381a00f5e
Samuel Vargas <sjvc0595@gmail.com>
2025-05-24 19:22:54 -0500

    feat(user): Implement user registration and retrieval endpoints

--- Laboratory1/1011201389/app.py
+++ Laboratory1/1011201389/app.py
@@ -1,22 +1,51 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flasgger import Swagger
+
+# Validators
+from handlers.register.register_handlers import NameHandler, PreferredChannelHandler, AvailableChannelsHandler
+
+# Database
+from database.database import database_users
+
+# Models
+from user import User
 
 app = Flask(__name__)
 swagger = Swagger(app)
+
+# Validate user data definition
+validator_user_data = NameHandler(PreferredChannelHandler(AvailableChannelsHandler()))
 
 # Enpoints definition
 
 # Register users
 @app.post('/users')
 def register_user():
-    pass
+    """
+    Create a user
+    ---
+    """
+    data = request.get_json()
+    try:
+        validator_user_data.handle(data)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    
+    # Create user
+    user = User.from_dict(data)
+    database_users.append(user)
+
+    return jsonify(data), 201
 
 # Get users
 @app.get('/users')
 def get_users():
-    pass
+    return jsonify([user.to_dict() for user in database_users]), 200
 
 # Send a notification
 @app.route('/notifications/send')
 def send_notification():
     pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
