bc8ea1cd39b7872e30d6d953c69c34f4b4b892f8
Samuel Vargas <sjvc0595@gmail.com>
2025-05-26 12:03:13 -0500

    feat(api): Refactor Notification class to include from_dict and to_dict methods

--- Laboratory1/1011201389/notification.py
+++ Laboratory1/1011201389/notification.py
@@ -3,3 +3,18 @@ class Notification():
         self.user_name = user_name
         self.message = message
         self.priority = priority
+
+    @classmethod
+    def from_dict(cls, data):
+        user_name = data.get("user_name")
+        message = data.get("message")
+        priority = data.get("priority")
+
+        return Notification(user_name, message, priority)
+    
+    def to_dict(self):
+        return {
+            "user_name": self.user_name,
+            "message": self.message,
+            "priority": self.priority
+        }
