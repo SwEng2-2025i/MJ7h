f63134209befd3f615af099ce57be65c60f741ea
Samuel Vargas <sjvc0595@gmail.com>
2025-05-26 12:03:08 -0500

    feat(api): Implement handlers for user name, message, and priority validation

---
+++ Laboratory1/1011201389/handlers/notification/notification_handlers.py
@@ -0,0 +1,28 @@
+from ..base_handler import BaseHandler
+
+class UserNameHandler(BaseHandler):
+    def handle(self, data):
+        user_name = data.get("user_name")
+
+        # Test if it's a non-empty string
+        if not isinstance(user_name, str) or not user_name.strip():
+            raise ValueError("'user_name' must be a non-empty string")
+        return super().handle(data)
+    
+class MessageHandler(BaseHandler):
+    def handle(self, data):
+        message = data.get("message")
+
+        # Test if it's a non-empty string
+        if not isinstance(message, str) or not message.strip():
+            raise ValueError("'message' must be a non-empty string")
+        return super().handle(data)
+    
+class PriorityHandler(BaseHandler):
+    def handle(self, data):
+        priority = data.get("priority")
+
+        # Test if it's a non-empty string
+        if not isinstance(priority, str) or not priority.strip():
+            raise ValueError("'priority' must be a non-empty string")
+        return super().handle(data)
