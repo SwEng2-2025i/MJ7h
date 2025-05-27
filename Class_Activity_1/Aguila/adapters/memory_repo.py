4def64910ef714769e07a85db2dd8d7812292911
srestrero <srestreporo@unal.edu.co>
2025-05-27 08:27:57 -0500

    add put endpoint to mark a task as done

---
+++ Class_Activity_1/Aguila/adapters/memory_repo.py
@@ -0,0 +1,19 @@
+from domain.ports import TaskOutputPort
+from domain.entities import Task
+
+class InMemoryTaskRepository(TaskOutputPort):
+    def __init__(self):
+        self.tasks = []
+
+    def save(self, task: Task) -> None:
+        self.tasks = [t for t in self.tasks if t.id != task.id]
+        self.tasks.append(task)
+
+    def list_all(self) -> list[Task]:
+        return self.tasks
+
+    def get_by_id(self, task_id: str) -> Task:
+        for task in self.tasks:
+            if task.id == task_id:
+                return task
+        return None
