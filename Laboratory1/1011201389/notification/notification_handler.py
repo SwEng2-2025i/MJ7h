f9b5fa7522dee3c64d56de8554f620493c805e70
Samuel Vargas <sjvc0595@gmail.com>
2025-05-28 21:14:08 -0500

    feat(notification): Enhance logging for notification handling with detailed messages

--- Laboratory1/1011201389/notification/notification_handler.py
+++ Laboratory1/1011201389/notification/notification_handler.py
@@ -10,7 +10,7 @@ class NotificationHandler():
     def handle(self, user, notification):
         if self.next_handler:
             return self.next_handler.handle(user, notification)
-        self.log(f"Notification could not be sent to {user.name}: {notification}")
+        self.log(f"Notification could not be sent to {user.name} vian any channel: {notification.message} (Priority: {notification.priority})")
         raise ConnectionError("No handler available to process the notification")
 
     def log(self, message):
@@ -18,3 +18,13 @@ class NotificationHandler():
 
     def will_be_successful(self):
         return choice([True, False])
+    
+    @property
+    def channel(self):
+        return self.__class__.__name__.replace("Handler", "").lower()
+    
+    def log_success(self, user, notification):
+        self.log(f"Notification sent successfully to {user.name} via {self.channel}: {notification.message} (Priority: {notification.priority})")
+    
+    def log_failure(self, user, notification):
+        self.log(f"Failed to send notification to {user.name} via {self.channel}: {notification.message} (Priority: {notification.priority})")
