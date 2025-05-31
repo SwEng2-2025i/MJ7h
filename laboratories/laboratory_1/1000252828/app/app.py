cbdb393e98c6e80daccb81e8f57ee12f121f1ca9
dfadames <dadames@unal.edu.co>
2025-05-30 22:14:40 -0500

    0.4.1 hotfix

--- laboratories/laboratory_1/1000252828/app/app.py
+++ laboratories/laboratory_1/1000252828/app/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from models.user_store import UserStore
-from channels.builder import builds_chain
+from channels.builder import build_chain
 from extra.logger import logger
 import random
 
 app = Flask(__name__)
 user_store = UserStore()
-# tes primero
+
 @app.route('/users', methods=['GET'])
 def list_users():
     users = user_store.list_users()
