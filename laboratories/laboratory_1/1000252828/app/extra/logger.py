dd7aacf41f88c4a52363ec0d374664fd1a0071b0
dfadames <dadames@unal.edu.co>
2025-05-30 21:58:35 -0500

    0.2.0 logger

---
+++ laboratories/laboratory_1/1000252828/app/extra/logger.py
@@ -0,0 +1,27 @@
+class NotificationLogger:
+    """Clase singleton para registrar intentos de notificación.
+    """
+    _instance = None
+    def __new__(cls):
+        # Verifica si ya existe una instancia (por lo del singleton)
+        if cls._instance is None:
+            cls._instance = super(NotificationLogger, cls).__new__(cls)
+            cls._instance.records = []
+        return cls._instance
+    #intento de loguear un mensaje de notificación
+    def log_attempt(self, user_name, channel, message, success):
+        record = {
+            'user': user_name,
+            'channel': channel,
+            'message': message,
+            'success': success
+        }
+        # Agregar el registro a la lista de registros
+        self.records.append(record)
+        print(f"[LOG] Usuario: {user_name}, Canal: {channel}, Éxito: {success}")
+    # Obtiene todos los registros de notificación
+    def get_logs(self):
+        return list(self.records)
+
+# Instancia global accesible directamente
+logger = NotificationLogger()
