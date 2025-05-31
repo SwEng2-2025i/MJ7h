49fe0eb49d6a7c64c5cd71ed0060d1dbebd258c1
dfadames <dadames@unal.edu.co>
2025-05-30 22:15:50 -0500

    0.5.0 create users endpoint

--- laboratories/laboratory_1/1000252828/app/app.py
+++ laboratories/laboratory_1/1000252828/app/app.py
@@ -6,8 +6,28 @@ import random
 
 app = Flask(__name__)
 user_store = UserStore()
-
+# obtener usuarios
 @app.route('/users', methods=['GET'])
 def list_users():
     users = user_store.list_users()
     return jsonify({'users': users}), 200
+
+# api de users para registrar un usuario
+@app.route('/users', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    name = data.get('name')
+    preferred = data.get('preferred_channel')
+    available = data.get('available_channels', [])
+
+    if not name or not preferred or not available:
+        return jsonify({'error': 'Falta nombre, canal preferido o canales disponibles'}), 400
+
+    if preferred not in available:
+        return jsonify({'error': 'El canal preferido debe estar en la lista de disponibles'}), 400
+
+    success = user_store.add_user(name, preferred, available)
+    if not success:
+        return jsonify({'error': 'Usuario ya existe'}), 400
+
+    return jsonify({'message': f'Usuario {name} registrado.'}), 201
