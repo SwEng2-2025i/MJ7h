991710f7c1ffd57ddbe880de8a3f2fd0745048a8
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2025-06-20 18:17:54 -0500

    Generando reportes de pruebas de integracion de backend

--- Class_Activity_2/1034657217/Test/BackEnd-Test.py
+++ Class_Activity_2/1034657217/Test/BackEnd-Test.py
@@ -61,53 +61,59 @@ def integration_test():
     # Preparte for integration test report
     results = []
     start_time = time.time()
-    #try:
-    # Step 1: Create user
-    test_start = time.time()
-    user_id = create_user("Camilo")
-    details = f"User created with ID: {user_id}"
-    results.append(results_template("Create User", "PASS", test_start,details))
-        
-
-    # Step 2: Create task for that user
-    test_start = time.time()
-    task_id = create_task(user_id, "Prepare presentation")
-    details = f'Task created with ID: {task_id}'
-    results.append(results_template("Create Task", "PASS", test_start,details ))
-
-    # Step 3: Verify that the task is registered and associated with the user
-    test_start = time.time()
-    tasks = get_tasks()
-    user_tasks = [t for t in tasks if t["user_id"] == user_id]
-    assert any(t["id"] == task_id for t in user_tasks), "❌ The task was not correctly registered"
-    print("✅ Test completed: task was successfully registered and linked to the user.")
-    details = f'Task {task_id} verified in user tasks list'
-    results.append(results_template("Verify Task Creation", "PASS", test_start, details))
-
-
-    #step 4: Delete the task
-    test_start = time.time()
-    delete_task(task_id)
-    new_tasks = get_tasks()
-    assert not any(t["id"] == task_id for t in new_tasks), "❌ The task was not deleted"
-    print("✅ Task deleted successfully.")
-    details = f'Task {task_id} deleted successfully'
-    results.append(results_template("Delete Task", "PASS", test_start,details))
-
-
-    # Step 5: Delete the user
-    test_start = time.time()
-    delete_user(user_id)
-    details = f'User {user_id} deleted successfully'
-    results.append(results_template("Delete User", "PASS", test_start,details))
     try:
-        deletion_confirm = get_user(user_id)
-        print("❌ User deletion failed: user still exists.")
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 404:
-            print("✅ User deletion confirmed: user not found.")
+        # Step 1: Create user
+        test_start = time.time()
+        user_id = create_user("Camilo")
+        details = f"User created with ID: {user_id}"
+        results.append(results_template("Create User", "PASS", test_start,details))
+            
 
-    print("✅ All integration tests passed successfully.")
+        # Step 2: Create task for that user
+        test_start = time.time()
+        task_id = create_task(user_id, "Prepare presentation")
+        details = f'Task created with ID: {task_id}'
+        results.append(results_template("Create Task", "PASS", test_start,details ))
+
+        # Step 3: Verify that the task is registered and associated with the user
+        test_start = time.time()
+        tasks = get_tasks()
+        user_tasks = [t for t in tasks if t["user_id"] == user_id]
+        assert any(t["id"] == task_id for t in user_tasks), "❌ The task was not correctly registered"
+        print("✅ Test completed: task was successfully registered and linked to the user.")
+        details = f'Task {task_id} verified in user tasks list'
+        results.append(results_template("Verify Task Creation", "PASS", test_start, details))
+
+
+        #step 4: Delete the task
+        test_start = time.time()
+        delete_task(task_id)
+        new_tasks = get_tasks()
+        assert not any(t["id"] == task_id for t in new_tasks), "❌ The task was not deleted"
+        print("✅ Task deleted successfully.")
+        details = f'Task {task_id} deleted successfully'
+        results.append(results_template("Delete Task", "PASS", test_start,details))
+
+
+        # Step 5: Delete the user
+        test_start = time.time()
+        delete_user(user_id)
+        
+        try:
+            deletion_confirm = get_user(user_id)
+            print("❌ User deletion failed: user still exists.")
+            details = f'User {user_id} was not deleted'
+            results.append(results_template("Delete User Error", "FAIL", test_start,details))
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 404:
+                print("✅ User deletion confirmed: user not found.")
+                details = f'User {user_id} deleted successfully'
+                results.append(results_template("Delete User", "PASS", test_start,details))
+
+        print("✅ All integration tests passed successfully.")
+    except Exception as e:
+        print(f"❌ An error occurred during the integration test: {e}")
+        results.append(results_template("Integration Test", "FAIL", time.time() - start_time, str(e)))
 
     generator = TestReportGenerator()
     generator.generate_report(results,"Backend Integration Test Report")
