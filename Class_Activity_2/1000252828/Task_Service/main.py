9f6225878b0880cb4b20f84b43e0afe5cceb455b
dfadames <dadames@unal.edu.co>
2025-06-21 20:39:50 -0500

    1.0.2 delete working service and main

--- Class_Activity_2/1000252828/Task_Service/main.py
+++ Class_Activity_2/1000252828/Task_Service/main.py
@@ -38,6 +38,23 @@ def get_tasks():
     tasks = Task.query.all()
     return jsonify([{'id': t.id, 'title': t.title, 'user_id': t.user_id} for t in tasks])
 
+#Obtener tarea
+@service_b.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        return jsonify({'id': task.id, 'title': task.title, 'user_id': task.user_id})
+    return jsonify({'error': 'Task not found'}), 404
+#Eliminar tarea misma logica main
+@service_b.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return jsonify({'message': 'Task deleted successfully'}), 200
+    return jsonify({'error': 'Task not found'}), 404
+
 if __name__ == '__main__':
     with service_b.app_context():
         db.create_all()
